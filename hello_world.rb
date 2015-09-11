df3dac7b0686a37e8449a6dec463c6c035e77602
David Garber <davidgarber@gmail.com>
2015-09-11 15:06:58 -0700

    Ruby test exercism.

---
+++ hello_world.rb
@@ -0,0 +1,14 @@
+class HelloWorld
+  def self.hello(name = "World")
+    "Hello, #{name}!"
+  #  if name == "Alice"
+  #    'Hello, Alice!'
+  #  elsif name == "Bob"
+  #    'Hello, Bob!'
+  #  elsif name == ""
+  #    'Hello, !'
+  #  else name == "World"
+  #    'Hello, World!'
+  #  end
+  end
+end
